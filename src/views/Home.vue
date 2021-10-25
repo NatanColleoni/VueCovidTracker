24798a1dcca78516bd68a226d174b5ba003ec35a
NatanColleoni <natancolleonifonseca@gmail.com>
2021-10-25 15:39:06 -0300

    Covid Tracker

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+
+    <CountrySelect @get-country="getCountryData" :countries="countries"/>
+
+    <DataBoxes :stats="stats" />
+
+    <button @click="clearCountryData" v-if="stats.Country" class="bg-blue-700 text-white rounded p-3 mb-10 mt-5 focus:outline-none hover:bg-blue-500">
+      Clear Country
+    </button>
+  </main>
+
+  <main v-else> still loading </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataTitle from '@/components/DataTitle'
+import DataBoxes from '@/components/DataBoxes'
+import CountrySelect from '@/components/CountrySelect'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    DataTitle,
+    DataBoxes,
+    CountrySelect
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: []
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    },
+    getCountryData(country) {
+      this.stats = country
+      this.title = country.Country
+    },
+    async clearCountryData() {
+      this.loading = true
+      const data = await this.fetchCovidData()
+      this.title = 'Global'
+      this.stats = data.Global
+      this.loading = false
+    }
+  },
+  async created(){
+    const data = await this.fetchCovidData()
+    
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
   }
-}
+};
 </script>
